3535f8c3c152987d8d60045fa3e589aeb2975176
Mark Otweyo <markotweyo99@gmail.com>
2021-11-16 01:08:56 +0300

    Added UPDATE user route with JWT

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
 const authRoute = require('./routes/auth')
+const userRoute = require('./routes/users')
 
 dotenv.config()
 
@@ -18,6 +19,7 @@ app.use(express.json())
 
 
 app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
 
 
 
