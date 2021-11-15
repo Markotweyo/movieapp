770cb38e67a549aed3de39d41341aea100ab491a
Mark Otweyo <markotweyo99@gmail.com>
2021-11-15 17:52:22 +0300

    Added REGISTER auth route

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,16 +1,25 @@
 const router = require ('express').Router();
 const User = require ('../models/User');
 
-router.post('/register', (req, res)=>{
+//REGISTER
+router.post('/register', async (req, res)=>{
     const newUser= new User ({
         username: req.body.username,
         email: req.body.email,
         password: req.body.password,
     });
 
-    const user = newUser.save();
-    res.json(user)
+    try {
+        const user = await newUser.save();
+        res.status(201).json(user)
+        
+    } catch (error) {
+        res.status(500).json(err)
+        
+    }
+
+  
     
 });
 
-module.exports = router
+module.exports = router;
---
+++ api/routes/lists.js
@@ -0,0 +1,6 @@
+const router= require('express').Router()
+const List = require('../models/List');
+
+router.get('/list' , (req, res)=>{
+    
+})
